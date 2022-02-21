48d262cdcb8924e2c42f94bc2d7a0b023ca1d552
miracleexotic <mai_nutthawat@hotmail.com>
2022-02-22 00:07:32 +0700

    close #4 - add entity ManageSalary

---
+++ backend/entity/manage_salary.go
@@ -0,0 +1,66 @@
+package entity
+
+import (
+	"time"
+
+	"github.com/asaskevich/govalidator"
+	"gorm.io/gorm"
+)
+
+type Assessment struct {
+	gorm.Model
+	Level uint
+	Name  string
+
+	ManageSalarys []ManageSalary `gorm:"foreignkey:AssessmentID"`
+}
+
+type BonusStatus struct {
+	gorm.Model
+	Name string
+
+	ManageSalarys []ManageSalary `gorm:"foreignkey:BonusStatusID"`
+}
+
+type ManageSalary struct {
+	gorm.Model
+
+	ManagerID *uint
+	Manager   Employee `gorm:"references:ID" valid:"-"`
+
+	ManageWorkTimeID *uint
+	ManageWorkTime   ManageWorkTime `gorm:"references:ID;constraint:OnDelete:CASCADE" valid:"-"`
+
+	AssessmentID *uint
+	Assessment   Assessment `gorm:"references:ID" valid:"-"`
+
+	BonusAmount float64 `valid:"customPositiveNumber~BonusAmount must be positive number"`
+	BonusDetail string  `valid:"stringlength(0|200)~BonusDetail length must be between 0 - 200"`
+
+	BonusStatusID *uint
+	BonusStatus   BonusStatus `gorm:"references:ID" valid:"-"`
+
+	CreateAt time.Time `valid:"present~CreateAt must be present"`
+}
+
+// Custom validation
+func init() {
+	govalidator.CustomTypeTagMap.Set("customPositiveNumber", func(i interface{}, context interface{}) bool {
+		return i.(float64) >= 0
+	})
+
+	govalidator.CustomTypeTagMap.Set("past", func(i interface{}, context interface{}) bool {
+		t := i.(time.Time)
+		return t.Before(time.Now())
+	})
+
+	govalidator.CustomTypeTagMap.Set("future", func(i interface{}, context interface{}) bool {
+		t := i.(time.Time)
+		return t.After(time.Now())
+	})
+
+	govalidator.CustomTypeTagMap.Set("present", func(i interface{}, context interface{}) bool {
+		t := i.(time.Time)
+		return t.After(time.Now().Add(time.Hour*-12)) && t.Before(time.Now().Add(time.Hour*12))
+	})
+}
